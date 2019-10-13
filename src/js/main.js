7420b74cb9a1c4c921355ab6cad6e73a2d7342ec
islam-404 <islamkompaev@mail.ru>
2019-10-13 16:50:58 +0300

    00

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,14 @@
 var button = document.querySelector('#button');
 var modal = document.querySelector('#modal');
 var close = document.querySelector('#close');
+var closeTime = function(){modal.classList.remove('modal_active')}
 
 button.addEventListener('click', function() {
     modal.classList.add('modal_active');
+    setTimeout(closeTime, 50000);
 });
 
 close.addEventListener('click', function(){
     modal.classList.remove('modal_active');
 });
+
